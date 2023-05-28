f79a1f09ea1260f8a0da00f3e125cdf516dcc269
Kieran Jaggernauth <thejugg63@gmail.com>
2023-05-28 02:15:46 -0500

    started vue migration

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+export default {
+  name: "App",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<template>
+  <h1>Test</h1>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  name: "Landing",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<template>
+  <div>
+    <h1>Landing</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
